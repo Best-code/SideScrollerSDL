ebe5b97df7a3af5bfc6ef6eb90494f30c2801833
best-code <cpmraygun@gmail.com>
2023-10-09 23:10:57 -0400

    Initial Commit

---
+++ SDL_2023/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SDL_2023
+//
+//  Created by Colin Maloney on 10/9/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
